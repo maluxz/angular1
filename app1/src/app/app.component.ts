3d03af17c9dbb396e6b76bf5f3d773a621cc95c6
Mario <malux97@hotmail.com>
2023-10-26 09:46:53 -0600

    Ejercicios 5, 6 y 7 de AngularYA

--- app1/src/app/app.component.ts
+++ app1/src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app1';
+
+  nombre = '';
+  apellido='';
+  edad = 26;
+  sueldos = [1500, 2500, 6000];
+
+  contador = 1;
+  incrementar() {
+    this.contador++;
+  }
+  decrementar() {
+    this.contador--;
+  }
 }
